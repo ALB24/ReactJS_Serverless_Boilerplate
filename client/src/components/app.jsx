fe6321b72a53bff6e91717c6371f9aae31dd8684
ALB24 <universidad.aless@gmail.com>
2018-08-26 17:17:16 -0500

    Initial Setup

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -4,7 +4,7 @@ export default class App extends Component {
 	render () {
 		return (
 			<div>
-				<h1>Maggie Louise Confections</h1>
+				<h1>ReactJS_Serverless_Boilerplate</h1>
 			</div>
 		);
 	}
